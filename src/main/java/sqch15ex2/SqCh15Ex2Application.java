cf469bee995cf320603657c50670a127b112f165
osparking <jbpark03@gmail.com>
2023-11-11 15:28:45 +0900

    최초 커밋 - 종속성: lombok, web

---
+++ src/main/java/sqch15ex2/SqCh15Ex2Application.java
@@ -0,0 +1,13 @@
+package sqch15ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh15Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh15Ex2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sqch15ex2/SqCh15Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sqch15ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh15Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
