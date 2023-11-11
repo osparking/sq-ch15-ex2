871bff52a6217e8fec6f8e0c83dae2de5f99bd06
osparking <jbpark03@gmail.com>
2023-11-11 23:02:38 +0900

    로그인 실패 처리 결과 검증 단위시험

--- src/test/java/sqch15ex2/controller/logincontroller/LoginProcessTest.java
+++ src/test/java/sqch15ex2/controller/logincontroller/LoginProcessTest.java
@@ -16,7 +16,7 @@ import sqch15ex2.controller.LoginController;
 import sqch15ex2.controller.LoginProcessor;
 
 @ExtendWith(MockitoExtension.class)
-@DisplayName("로그인 액션 메소드 - 로그인 성공 처리 시험")
+@DisplayName("로그인 POST 방법 요청 처리 액션 검증")
 class LoginProcessTest {
 
 	@Mock
@@ -29,6 +29,18 @@ class LoginProcessTest {
 	private LoginController loginController;
 
 	@Test
+	@DisplayName("로그인 액션 메소드 - 로그인 실패 처리 시험")
+	void loginFailureTest() {
+		given(loginProcessor.isLoggedIn()).willReturn(false);
+		String viewName = loginController.loginProcess(model, "username",
+				"password");
+		assertEquals("login.html", viewName);
+		verify(model)
+		.addAttribute("loginStatusMsg", "로그인 하셔야 됩니다.");		
+	}
+	
+	@Test
+	@DisplayName("로그인 액션 메소드 - 로그인 성공 처리 시험")
 	void loginSuccessTest() {
 		given(loginProcessor.isLoggedIn()).willReturn(true);
 
