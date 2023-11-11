4d4d0d2c7beafed61b223b57774b0abec2344344
osparking <jbpark03@gmail.com>
2023-11-11 22:45:52 +0900

    시험 대상 메소드를 호출함

--- src/test/java/sqch15ex2/controller/logincontroller/LoginProcessTest.java
+++ src/test/java/sqch15ex2/controller/logincontroller/LoginProcessTest.java
@@ -1,6 +1,8 @@
 package sqch15ex2.controller.logincontroller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,16 +19,20 @@ class LoginProcessTest {
 
 	@Mock
 	private Model model;
-	
-	@Mock 
+
+	@Mock
 	private LoginProcessor loginProcessor;
-	
+
 	@InjectMocks
 	private LoginController loginController;
-	
+
 	@Test
 	void loginSuccessTest() {
 		given(loginProcessor.isLoggedIn()).willReturn(true);
+
+		String username = "natalie";
+		String viewName = loginController.loginProcess(model, username,
+				"password");
 	}
 
 }
